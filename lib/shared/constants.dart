79948896e0e60d08eb5ec37ee4d72f324b183fd7
Karthik-Kollur <karthik.kollur.jsr@gmail.com>
2023-04-29 10:55:49 +0530

    My First Commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:687874303519:web:82e5ae3b88f75f3bfdd72a";
+  static String apiKey = "AIzaSyAB4esZlrb4SoWcl4lOKUGVhG0jX-bHBes";
+  static String messagingSenderId = "687874303519";
+  static String projectId = "chatappflutter-dbeda";
+  final primaryColor = const Color(0xFFee7b64);
+}
